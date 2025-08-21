f58f9ff451912b0366910473fc38e8f6aaf8dfbf
erikanderson-lang <erik.anderson@ucas-edu.net>
2025-08-21 09:52:40 -0600

    variable notes

--- notes/hello world.py/hello_world.py
+++ notes/hello world.py/hello_world.py
@@ -1,3 +1,5 @@
 #EA 1st hello world.py
 name = input("what is your name: ")
 print("Hello Comrade",name)
+lastname = input("what is your last name: ")
+print("cool, what is your favorate season: ")
---
+++ variables.py
@@ -0,0 +1,9 @@
+#EA 1st Variables notes
+#a way to store info in a program to later acsses and change it
+
+#Syntax
+#name = content 
+#name is spasific no space _ start with letter or _ no special charecters
+# is = asignment oporator (yes = yes) 
+#content 
+#declaration is name of veriable
