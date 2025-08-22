e5a8e09e52c2038d9482e44ba4985d25ab82ff98
erikanderson-lang <erik.anderson@ucas-edu.net>
2025-08-22 09:44:27 -0600

    who_are_you.py

--- who_are_you.py
+++ who_are_you.py
@@ -1,6 +1,6 @@
 # f f5f f f5f f f5f f565f f f5f f f5f f f5f f565f f f5f f f5f f f5f f565f f f5f f f5f f f5f f565f f56787  5f67 f 
-name = input("what is your name: ") 
-age = input("what is your age: ") 
-favcollour = ('what is )
+name = input("what is your name comrade: ") 
+age = input("what is your age comrade: ") 
+fav_collour = input("what is your favourate collour comrade: ")
 
-print("Hello",name)
+print("Hello comrade",name, "age is",age, "and favourate collour is",fav_collour)
