8e84c6fb731db46104a96fdf509a2c0ac8ba90e0
erikanderson-lang <erik.anderson@ucas-edu.net>
2025-08-27 09:52:42 -0600

    notes

---
+++ notes/flotes.py
@@ -0,0 +1,24 @@
+#EA 1st intagers flotes
+#intagers
+e = 1112
+
+#flote
+pi = 3.14159265357989323846424842
+damage = 21
+
+#MATH
+print(5+2)#7
+print(5-2)#3
+print(5*2)#10
+print(5/2)#2.5
+print(5//2)#2
+print(5%2)#1
+print(5**2)#25
+#type casting
+num = int(3.14159265357989323846424842) #no rounding
+# using variables
+hours = 3
+min = 4
+print(hours*60+min)
+age = int(input("your age: \n"))
+print("oof", 525600*age )
