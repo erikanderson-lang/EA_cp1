98cfcc50a83a440b09a7cba73d7c3e8c4f926c0e
erikanderson-lang <erik.anerson@ucas-edu.net>
2025-09-02 09:52:58 -0600

    EA_cp1

---
+++ notes/string.py
@@ -0,0 +1,16 @@
+#EA 1s string notes
+
+# a quotstion of symbols in ""
+#losly held to gather is sring
+#string = "content of string" = 'content of string'
+#len()
+#escape chr \t tab \n next line
+
+#concatinate
+#string_one + string_two
+first_name = ("Erik")
+full_name = first_name + " " + "anderson"
+print(full_name)
+
+#index
+print(first_name[2:-0])
