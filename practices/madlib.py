e7ff9b789b62c5337036ef2fe13bc5c01b8417e4
erikanderson-lang <erik.anerson@ucas-edu.net>
2025-09-03 09:53:26 -0600

    EA_cp1 madlib

---
+++ practices/madlib.py
@@ -0,0 +1,7 @@
+#EA 1st madlib
+adjective = input("I need an adjective (With -ing): ")
+name = input("I need a name: ")
+noun = input("I need a noun (plural): ")
+room = input ("A room: ")
+name2 = input("Second name: ")
+print("In north swiserland, " + name + " woke up " + adjective + " and decided to invade poland using 100 " + noun + ". " + name + " went to the " + room +" planing room with " + name2 + " and planed to plan. The next day, " + name + " and " + name2 + " gave up and went to the soviet union to eat " + adjective + " pork. Then germnay invaded poland for a third time but " + name2 + " became leader of poland and destroyed the mongoulian empire.")
