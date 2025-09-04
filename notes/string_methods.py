d30d5bec50823e0877ec916104be293fd3c9642b
erikanderson-lang <erik.anerson@ucas-edu.net>
2025-09-04 09:51:24 -0600

    EA_cp1

---
+++ notes/string_methods.py
@@ -0,0 +1,22 @@
+#EA-cp1
+# What is a method? a way to alter my sting i a in a a certain way          print(subject.upper())
+# How do I write a method? print(subject.upper()) 
+# What methods can I use to change the formatting of my string? f-string
+# What methods allow me to search the contents of my string? 
+# What methods allow me to change the contents of my string? .format()
+# Why is it important to know what type of output a method will give me? True or false index get the right names and suff
+
+
+
+
+collour = input("collor: ").strip().lower().capitalize().title()
+print("cool " + collour + " is cool.")
+# f-string
+print(f"that is cool {collour}")
+pi=3.14159265357989323846448
+
+#humuhumunuknukuapwaaa
+#antidisensablishmentarianism
+#hipopoomonostropicsquepadilaphobia
+#chugerugerugerchuermugerchugerugerug
+#phnumaultramicroscopicsillicovolcanoconiosis
