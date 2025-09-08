5d307cdad376eb706ed3e04250ab0251e6c43320
erikanderson-lang <erik.anerson@ucas-edu.net>
2025-09-08 09:36:38 -0600

    EA_cp1 formating outputs notes

---
+++ notes/formatting_outputs.py
@@ -0,0 +1,19 @@
+#EA_cp1 formating outputs notes
+
+#How do I write the format method?
+#print("hallo {}, you are {:,}{:E}{:b}" (name, age)) money = $25.06 Print("you have ${:.2f} money") {} {:,} {:E} {:b} {:.2f} {:%}
+
+#What does the format method allow me to change about my outputs?
+#commas exponens binay decimals percents
+
+#Describe why it is important to format your outputs
+#so it is more clear
+
+#What is an f-string?
+#a better way to format
+
+#How do I create and f-string?
+pint(f"Hallo {name}, you are{age} ")
+
+#What do f-strings allow me to do?
+#format my outputs
