5ee1a23ce680f5ba58ea2f72351cf9d3de1a0569
erikanderson-lang <erik.anerson@ucas-edu.net>
2025-09-10 09:41:08 -0600

    rand_number_notes.py

---
+++ notes/rand_number_notes.py
@@ -0,0 +1,29 @@
+#EA_CP1 Random Numbers Notes
+
+#Can computers really generate a "random" number?
+#no
+
+#What is a python library?
+#a collection of pre-built unctions that we can pull into our code 
+
+#What are functions?
+#an algorithom that do something
+
+#How do you call a function?
+#name a function
+
+#What have we already learned that is similar to a function?
+#methods are simalar to functions but methods need to be part of a data type
+
+#What function to I call to get a random number?
+#random.random    random.randint
+
+#What are arguments?
+#the numbers we put in our function
+
+#What arguments are needed to generate a random number?
+#two numbers
+
+import random
+print(random.random())
+print(random.randint(1900,2000))
