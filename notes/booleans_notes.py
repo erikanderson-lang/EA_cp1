f7bfb5c0441096d5d4b59c4619cf8656da325e7a
erikanderson-lang <erikanderson@ucas-edu.net>
2025-09-18 09:51:54 -0600

    booleans_notes_stuff

--- notes/booleans_notes.py
+++ notes/booleans_notes.py
@@ -1,1 +1,27 @@
 # EA cp1 booleans                                                        
+#What can a Boolean hold? (i.e. what is a Boolean?)
+#A true or false statement
+#What are Booleans normally used for?
+#true or false statements
+#What does bool() let you do?
+#takes a input and makes it a boolean
+#What values are considered True? 
+#numbers
+#What values are considered False?
+#0
+#What are some python built-in functions that return a Boolean value?(Try and find 1 more than the example you are given!)
+#loops numbers alphabet
+#What are the simple data types?
+
+#What would you use each data type for?
+
+import time
+import datetime as date
+print(f"today is {}")
+#min. %M
+#weekday %A %a
+#day %d
+#mon. %b %b
+#MOn.num. %m
+#year %y, %y
+#sec. %s
