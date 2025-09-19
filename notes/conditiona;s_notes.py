f21093b30932695b0edf7b9c50b37271a6107eca
erikanderson-lang <erikanderson@ucas-edu.net>
2025-09-19 09:52:35 -0600

    conditionals_notes

---
+++ notes/conditiona;s_notes.py
@@ -0,0 +1,25 @@
+#EA CP1
+
+#What are conditionals in python?
+#A code that uses a boolean to decide what to run
+
+#How do we write an if statement? 
+#  if stuff (other stuff)
+
+#How do we write conditions?
+# if elif else
+
+#Why does the code need to be indented after the if?
+# so it is a conditional
+
+#Why do we want to have else statements?
+#so there is still more posibility
+
+#How many else if statements can you create? 
+#three
+
+#Why does the order of your statements matter in a conditional?
+#so it knows where to read
+
+#How do we use boolean instead of boolean statements? 
+#instead of a condition just write True
