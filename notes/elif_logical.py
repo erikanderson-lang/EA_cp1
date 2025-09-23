1b2a8d954e0ccd8ba089f179df058b732aac46dc
erikanderson-lang <erikanderson@ucas-edu.net>
2025-09-23 09:49:15 -0600

    elif notes

---
+++ notes/elif_logical.py
@@ -0,0 +1,23 @@
+#EAcp1
+
+#How do we check more than one condition in python?
+#elif
+
+#How many else if statements can you create? 
+#Infinate
+
+#Why does the order of your statements matter in a conditional?
+#so you dont get an error
+
+#How do we use boolean instead of boolean statements? 
+#
+
+#What are the logical operators?
+#and not or
+
+#How are logical operators used to change the outcome of a condition?
+#  and|true|true
+#   or|true|false
+#  not|false|
+#What does pass do?
+#makes something invalid
