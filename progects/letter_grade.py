6d88fe919e9d0b83d1337582d7fef8928e573ad7
erikanderson-lang <erikanderson@ucas-edu.net>
2025-09-24 09:52:07 -0600

    letter_grade

---
+++ progects/letter_grade.py
@@ -0,0 +1,17 @@
+#EA_CP1
+percent = int(input("what is your grade: "))
+
+if percent >= 90:
+    print("you have an A")
+elif percent >= 80:
+    print("you have an B")
+elif percent >= 70:
+    print("you have an C")
+elif percent >= 60:
+    print("you have an D")
+elif percent >= 50:
+    print("you have an F")
+elif percent >= 40:
+    print("you have an F")
+elif percent <= 40:
+    print("you have an F")
