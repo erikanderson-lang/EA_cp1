717ac39d7f9af0dda8a7d60eabc85dcdc0ada36c
erikanderson-lang <erikanderson@ucas-edu.net>
2025-09-25 09:53:41 -0600

    notes

---
+++ notes/program_design_notes.py
@@ -0,0 +1,22 @@
+
+
+#What is the software development life cycle?
+#The cycle of software development
+#What are the steps in the software development life cycle?
+#1 ask clarifying questions 2 assign people tasks 3 “do it” 4 test 5 release 
+#Why do we use the software development life cycle?
+#To do better
+#What are the 3 types of bugs that are most likely to occur in a program?
+#Syntax error - grammar error
+#Logic error - wrong steps
+#Run time error - stupin proof failiure
+#Where do each of these bugs come from?
+#Code errors
+#How do we resolve each type of bug?
+#Read the error message then fix the syntax error
+#Have a good plan
+
+
+#What is debugging?
+#fixing
+#Why do I need to be comfortable with debugging?
