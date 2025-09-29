6fea3c7db14a5e0f2150c5249c5c8c97b8035479
erikanderson-lang <erikanderson@ucas-edu.net>
2025-09-29 09:52:07 -0600

    list

---
+++ notes/list_notes.py
@@ -0,0 +1,28 @@
+#EAcp1
+
+#What are lists? 
+#
+
+#How do you find the length of a list?
+#{len(sibs)}
+
+#What data types can be put inside of lists?
+# strings
+
+#How do you get one item from a list?
+#
+
+#How do you replace a value in a list?
+#.pop()
+
+#What does append do?
+#stuff
+
+#What does insert do?
+#moves something in the lis to a different spot
+
+#What does extend do?
+#adds a list to the list
+
+#How do you remove an item from a list?
+#.remove("")
