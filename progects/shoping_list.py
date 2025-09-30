e6044a878e963e7f214f74f007e9a2eae5e079b4
erikanderson-lang <erikanderson@ucas-edu.net>
2025-09-30 09:51:09 -0600

    shoplist

---
+++ progects/shoping_list.py
@@ -0,0 +1,22 @@
+# EAcp1 class Shopping List Manager
+
+shoplist = []
+
+print (" hallo comrade this is your shopping list what do you need")
+while True:
+    print (" hallo comrade this is your shopping list what do you need")
+    action = input(" chose a item (1-4): ")
+    
+    if action == '1': 
+        item = input ("enter item: ")
+        shoplist.append(item)
+        print(f"'(item)' added")
+    
+    elif action == 2
+    item = input ("enter item: ")
+    shoplist.append(item)
+    print(f"'(item)' added")
+    
+    
+    if action == "exit":
+        break
