0d764293012932b4e62ea9a0960eea8212de8f00
erikanderson-lang <erikanderson@ucas-edu.net>
2025-10-06 09:52:45 -0600

    r p s

--- practices/rock paper scisors.py
+++ practices/rock paper scisors.py
@@ -1,8 +1,39 @@
-hang = """_____
-   |   0
-   |  /|\\
-   |   /\\
-   |
-   |________
+#Ea 1
+import time
+import random
+hang = """|___________
+|
+|
+|___________
+|
+|
+|____________
 """
 print (f"{hang}")
+
+print ("this is rock paper scisors comrade. you chose 1 = rock, 2 = paper, or 3 = scisors.")
+choise = input("chose 1-3: ")
+
+rock = 1
+paper = 2
+scissors = 3
+
+compchoise = random.randint(1,3)
+for num in range(3,0,-1):
+    print(num)
+    time.sleep(1)
+
+
+
+if compchoise == 1:
+    print("I chose rock")
+
+if compchoise == 2:
+    print("I chose paper")
+
+if compchoise == 3:
+    print("I chose scissors")
+
+if compchoise == choise: 
+    print ("We chose same go again")
+    continue
