65248449055498482d31aca2ed424dfbf70b9f62
erikanderson-lang <erikanderson@ucas-edu.net>
2025-10-08 09:44:11 -0600

    psudocode

---
+++ notes/psudocode.py
@@ -0,0 +1,61 @@
+#Ea cs1
+
+#What is pseudocode
+#code writen in plain english
+
+#Why do we write pseudocode? 
+#so any one can read it
+
+#How do you write pseudocode? 
+#english
+
+#What kinds of words to we swap out for keywords in python? 
+#print to show/display   elif to or else  add to update and reset 
+
+#How can it be helpful to try multiple versions of your pseudocode?
+#so very one can read the code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
