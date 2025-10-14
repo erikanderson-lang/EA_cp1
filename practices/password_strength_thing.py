0a2cda108dad295d8a84c2e553643bbcbdf22c4e
erikanderson-lang <erikanderson@ucas-edu.net>
2025-10-14 09:53:04 -0600

    No

--- practices/password_strength_thing.py
+++ practices/password_strength_thing.py
@@ -5,19 +5,20 @@ password = (input("какой y тебя пароль, товарищ?: "))
 #checking password for length
 while True:
     if len(password) >=8:
-        print("+1 pointt")
-    else: print("you need eight characters at least")
-    break
-time.sleep(1)
+        print("+1 point")
+    else: 
+        print("you need eight characters at least")
+    time.sleep(1)
+
     #checking password for capitals
 
     #checking password for a number
+    password = password
+    passwordnum = password.isalpha()
+    print("You need a number")
+    continue
+#checking password for lowercase
 if password == (f"{password}"):
-        password.find("1-9")
-else: 
-        print("you need one number at least")
-        time.sleep(1)
-    #checking password for lowercase
-if password == (f"{password}"):
-        password.find("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z")
-print("you need a lowercase letter" )
+    password.find("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z")
+else:
+    print("you need a lowercase letter" )
