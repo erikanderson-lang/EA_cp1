fc8209ad00a795b2ebed52489388ecb92ebed352
erik.anderson <erinanderson@ucas-edu.net>
2025-10-20 09:53:51 -0600

    functions notes

---
+++ notes/function_notes.py
@@ -0,0 +1,47 @@
+#EA
+
+#what is the porpous of a function?
+#they organise your code and make it reuseable
+
+#How do you wrght functions?
+#def add(x,y):
+#    print(f"{x}+{y} = {x+y}")
+
+#Why shoud functions be abstract?
+#so it can be reuseable
+
+#how do you increce the readability of program?
+#add functions
+
+#what does the modularity have to do with functions?
+#
+
+#how do arguments and parameters work togather?
+#
+
+#what are return statements? what do they do?
+#they replace the call with the answer
+
+
+def add(x,y):
+    print(f"{x}+{y} = {x+y}")
+
+def add(x,y):
+    return x+y
+
+add(5,5)
+print(f"10 + 72 = {add(10,72)}")
+x = 0
+while x < add(3,9):
+    print("duck")
+    x+=1 
+print("goose")
+
+add(3,9)
+
+def initials(name):
+    names = name.split(" ")
+    initials = ""
+    for name in names:
+        initials += name[0]
+    return initials
--- practices/password_strength_thing.py
+++ practices/password_strength_thing.py
@@ -3,22 +3,23 @@ import time
 #ask for password
 password = (input("какой y тебя пароль, товарищ?: "))
 #checking password for length
-while True:
-    if len(password) >=8:
+
+if len(password) >=8:
         print("+1 point")
-    else: 
+else: 
         print("you need eight characters at least")
-    time.sleep(1)
+time.sleep(1)
 
     #checking password for capitals
 
     #checking password for a number
-    password = password
-    passwordnum = password.isalpha()
+if password == password.isalpha():
+      print("placeholder")
+else:
     print("You need a number")
-    continue
-#checking password for lowercase
+
+    #checking password for lowercase
 if password == (f"{password}"):
-    password.find("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z")
+        password.find("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z")
 else:
     print("you need a lowercase letter" )
