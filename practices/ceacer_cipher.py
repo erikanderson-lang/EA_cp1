5912a027c7b20a551bc0ddf84421b2f303d1a73c
erik.anderson <erinanderson@ucas-edu.net>
2025-10-22 09:39:00 -0600

    Ea

---
+++ practices/ceacer_cipher.py
@@ -0,0 +1,7 @@
+#EA 1
+#user key choise
+phrase = input("What is the phrase comrade: ").lower()
+keyinput = float(input("key =: "))
+
+print(f"a = {ord("a")}")
+print(f"97 = {chr(97)}")
--- practices/password_strength_thing.py
+++ practices/password_strength_thing.py
@@ -1,25 +1,38 @@
 #EA cp1 
 import time
 #ask for password
-password = (input("какой y тебя пароль, товарищ?: "))
+password = (input("Password: "))
 #checking password for length
 
 if len(password) >=8:
-        print("+1 point")
+        print("+1a")
 else: 
         print("you need eight characters at least")
 time.sleep(1)
 
     #checking password for capitals
-
+if password == (f"{password}"):
+        password.find("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z")
+        print("+1b")
+else:
+    print("You need a uppercase letter")
     #checking password for a number
-if password == password.isalpha():
-      print("placeholder")
+if password == (f"{password}"):
+        password.find("1,2,3,4,5,6,7,8,9,0")
+        print("+1c")
 else:
     print("You need a number")
 
     #checking password for lowercase
 if password == (f"{password}"):
         password.find("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z")
+        print("+1d")
 else:
     print("you need a lowercase letter" )
+
+#checking for a special charicter
+if password == (f"{password}"):
+        password.find("!,@,#,$,%,^,&,*<(,),<,>,.,?,/,{,},|,-,_,+,=,`,~")
+        print("+1e")
+else:
+    print("you need a special charecter" )
