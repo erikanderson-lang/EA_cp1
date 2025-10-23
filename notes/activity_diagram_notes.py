398ff4561183021abd1e20ec9180eaea849462e2
erik.anderson <erinanderson@ucas-edu.net>
2025-10-23 09:41:06 -0600

    E

---
+++ notes/activity_diagram_notes.py
@@ -0,0 +1,34 @@
+#What does UML stand for?
+#undefined modeling language
+
+#When would you use an Activity Diagram?
+#when you are making code
+
+#What symbol indicates the start of an activity diagram?
+#a black circle
+
+#What symbol indicates an action in the activity diagram?
+#a rounded rectangle
+
+#What does a decision node do?
+#makes a choise
+
+#Does the direction of the arrows matter?
+#yes
+
+#What is a fork?
+#a piont where a thing could split to two different things
+
+#How is a join different from a merge?
+#a join joins and a merge choses and merges
+
+#What is a swimlane?
+#a line to seperate on an activity diagram
+
+#What do swimlanes do?
+#divides the actions between different parts
+
+#How do activity diagrams end?
+#two circles
+
+
