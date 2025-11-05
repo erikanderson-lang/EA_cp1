10f0e784251387013a7e3e0830d135c13769faa6
erik.anderson <erinanderson@ucas-edu.net>
2025-11-05 09:54:58 -0700

    RHIUSH E E FRF EA G SG A FAW

--- practices/maze.py
+++ practices/maze.py
@@ -3,22 +3,26 @@ import turtle as t
 import random
 
 m = t.Turtle()
-
+m.speed(1234567890)
 m.shape("square")
 #makes maze outline
-m.left(90)
-m.forward(400)
-m.left(90)
-m.forward(350)
 m.penup()
 m.forward(50)
 m.pendown()
+m.forward(550)
 m.left(90)
-m.forward(400)
+m.forward(600)
 m.left(90)
-m.forward(350)
+m.penup()
+m.forward(50)
+m.pendown()
+m.forward(550)
+m.pendown()
+m.left(90)
+m.forward(600)
 m.hideturtle()
-#makes the inside
+#makes the inside walls yes or no colloms and rows
+#hail
 gridrow = [
         [random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1)],
         [random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1)],
@@ -35,13 +39,36 @@ gridcol = [
         [random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1)],
         [random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1)]]
 
+#new toitle
+el = t.Turtle()
+el.shape("circle")
+el.speed(1234567890)
+
+def isgood():
+        pass
+# checks if there is a wall and prints if there is
+rock = 1
+while rock < 6:
+        if gridrow == 1:
+                el.pendown()
+                el.forward(100)
+                rock += 1
+
+        elif gridrow == 0:
+               el.penup()
+               el.forward(100)
+               rock += 1
 
 
-print (gridrow)
-print (gridcol)
+
+
+
+
+
+
+#checks to see if it is solvable
 
 t.done()
-def isgood():
 
 
 
@@ -63,5 +90,3 @@ def isgood():
 
 
 
-
-    t.done()
