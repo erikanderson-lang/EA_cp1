a148c8a1420da0d15c5a5d313bc8e3ee42e2d692
erikanderson-lang <erik.anderson@ucas-edu.net>
2025-11-18 09:52:43 -0700

    Comerade

---
+++ practices/flexible_calculator.py
@@ -0,0 +1,86 @@
+#EACP1
+import statistics as s
+print("""Welcome to the Flexible Calculator comerade
+Available operations are: sum, average, product""")
+operation = input("Which operation would you like to perform?: ").lower().strip()
+print(" ")
+nums1 = float (input("""Enter numbers (type 'done' when finished):
+Number: """))
+nums2 = float (input("Number: "))
+nums3 = float (input("Number: "))
+nums4 = float (input("Number: "))
+nums5 = float (input("Number: "))
+nums = (nums1,+ nums2, + nums3, + nums4, +nums5)
+if operation == "average":
+    for num in nums:
+        print (s.fmean[nums])
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("""comrade, steel production is low I said 
+      comerade, you must sleep on the ground I said
+      comerade, you must wipe off that froun or you will 
+      spend 
+      a
+      month 
+      in 
+      the 
+      gulags.
+    There is no fun in the U.S.S.R
+      There is no fun in the U.S.S.R""")
