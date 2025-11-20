e381a37f04912cb74cfe930da81b4d22187de8a5
erikanderson-lang <erik.anderson@ucas-edu.net>
2025-11-20 09:19:59 -0700

    FAST

--- practices/squared_nums.py
+++ practices/squared_nums.py
@@ -1,11 +1,5 @@
-#EACP1
-#numbers to be squared
+#EACP1 numbers to be squared and squares them
 nums = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
-#squares numbers
-def square(num):
-    return num*nums
-new_nums = map(lambda num: num*nums,nums)
-for num in new_nums:
-    print(num)
-    break
-
+new_nums = list(map(lambda n: (n*n),nums))
+print(f"origonal numbers{nums}")
+print(f"new numbers{new_nums}")
