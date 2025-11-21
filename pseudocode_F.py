b6ff620341f8e3d1132af8c9595c93b409771d58
erikanderson-lang <erik.anderson@ucas-edu.net>
2025-11-21 09:51:37 -0700

    WHAT

---
+++ pseudocode_F.py
@@ -0,0 +1,23 @@
+#EACP1
+
+# make the imports
+import random as r
+import turtle as t
+#define the words variable = what? 
+what = input(": ")
+#make if statement 
+
+    #if ___ is >,<= ___ then:
+if what > < = what:
+        #i variable blah blah __:
+    i += what
+        #more functions ()
+    i += what
+        #color change
+t.color("blue")
+#import what your random
+import random as r
+  # stupify to make all normal number
+what = input(int(": ")).strip
+#add to
+what + i
---
+++ turtle.py
@@ -0,0 +1,13 @@
+import turtle as t
+def draw_branch(length):
+    if length >5:
+        for i in range(3):
+            t.forward (length)
+            draw_branch(length/3)
+            t.backward (length)
+            t.right(60)
+
+tut = t.Turtle()
+tut.speed(2)
+
+
