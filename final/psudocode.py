06fe9e498bdc20b95d080b35430707d67f86f4ae
erikanderson-lang <erik.anderson@ucas-edu.net>
2025-12-01 09:38:25 -0700

    PSUDO_FINAL

---
+++ final/psudocode.py
@@ -0,0 +1,22 @@
+#EAcp1
+#saves loot dictionary 
+
+#saves stats dictionary 
+
+#saves inventory dictionary 
+
+#prints the problem and what is needed to do to win and how to lose then the game starts
+
+#gives them the option of going to the hall or looting before going to the hall and if they loot they get a random posible item from the loot table (dicionary)
+
+#when they go to the hall they have to fight the bad guys with different weapons including the weapons they have in their pack (saved as dictionary) after that they can turn right or left to go to different rooms (bridge or engine room)
+
+#in the hallways there is another loot room they can go to to get more things to help them survive and fight (and to have money for their score) there is always an option to go to a different room that they already cleared
+
+#if they go to the bridge they have to solve a sepcific puzzle so the alarm stops and their ship can be freed to go they then have to fight the way down the hallways to the hanger for bossfight on the way there are different security droids and one can drop a acsess card which can be used in the bossfight to acsess a crane to drop a container oh the other droids so there is less going on
+
+#gives puzzle and checks answer if wrong lets them try two more times before they have to wait one minute
+
+#else if they go to the engine room the have to solve a different puzzle to turn on the reverse engines giving them more time then they can go to the bridge looping back and letting them go fight the way down the hallways to the hanger for bossfight
+
+#if game is done lets them play again or quit and gives them their score (which is the money they earned plus the time they had left and the amount of enimies destroyed)
