fddfc061c7722f85e0c2f08fdc561de5b5982635
erikanderson-lang <erik.anderson@ucas-edu.net>
2025-12-02 09:53:04 -0700

    POIY

--- final/psudocode.py
+++ final/psudocode.py
@@ -1,22 +1,36 @@
 #EAcp1
 #saves loot dictionary 
+#stim pack 
+#t-47 blaster rifle
+#m-32 charge weapon
+#250 marks
+#50 marks
+#thermal detonator
 
 #saves stats dictionary 
+#health 20
+#wealth 50
+#acuracy 5 (damage addition)
 
 #saves inventory dictionary 
+#main hand(weapon) 1
+#backpack(stim pack) 2
+#backpack(thermal deatonator) 3
 
 #prints the problem and what is needed to do to win and how to lose then the game starts
 
 #gives them the option of going to the hall or looting before going to the hall and if they loot they get a random posible item from the loot table (dicionary)
 
-#when they go to the hall they have to fight the bad guys with different weapons including the weapons they have in their pack (saved as dictionary) after that they can turn right or left to go to different rooms (bridge or engine room)
-
-#in the hallways there is another loot room they can go to to get more things to help them survive and fight (and to have money for their score) there is always an option to go to a different room that they already cleared
-
-#if they go to the bridge they have to solve a sepcific puzzle so the alarm stops and their ship can be freed to go they then have to fight the way down the hallways to the hanger for bossfight on the way there are different security droids and one can drop a acsess card which can be used in the bossfight to acsess a crane to drop a container oh the other droids so there is less going on
-
-#gives puzzle and checks answer if wrong lets them try two more times before they have to wait one minute
+#when they go to the hall they have to fight the bad guys with different weapons including the weapons they have in their pack (saved as dictionary) after that they can turn right or left to go to different rooms (bridge or engine room) function for fight sequence against the security droids can be used later
 
 #else if they go to the engine room the have to solve a different puzzle to turn on the reverse engines giving them more time then they can go to the bridge looping back and letting them go fight the way down the hallways to the hanger for bossfight
+
+#in the hallways there is another loot room (which has same function as previous loot room accsesing dictionary in loot to let them chose to loot or skip and choose a loot item from loot dictionary exept for a weapon if they have it already) they can go to to get more things to help them survive and fight (and to have money for their score) there is always an option to go to a different room that they already cleared
+
+#if they go to the bridge they have to solve a sepcific puzzle so the alarm stops and their ship can be freed to go they then have to fight the way down the hallways to the hanger for bossfight on the way there are different security droids and one can drop a acsess card which can be used in the bossfight to acsess a crane to drop a container on the other droids so there is less going on
+
+#if there is sitm pack changes health stat in stats dictionary
+
+#gives puzzle and checks answer if wrong lets them try two more times before they have to wait one minute
 
 #if game is done lets them play again or quit and gives them their score (which is the money they earned plus the time they had left and the amount of enimies destroyed)
