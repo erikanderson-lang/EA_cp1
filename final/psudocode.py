947fcea5eaf4884ba77fbad5d332e91dfe7f432f
erikanderson-lang <erik.anderson@ucas-edu.net>
2025-12-03 09:53:12 -0700

    AEERTTERS

--- final/psudocode.py
+++ final/psudocode.py
@@ -17,20 +17,18 @@
 #backpack(stim pack) 2
 #backpack(thermal deatonator) 3
 
-#prints the problem and what is needed to do to win and how to lose then the game starts
+#prints the problem and what is needed to do to win and how to lose then the game starts (twenty munites to get out)
 
-#gives them the option of going to the hall or looting before going to the hall and if they loot they get a random posible item from the loot table (dicionary)
+#gives them the option of going to the hall or looting before going to the hall and if they loot. They get a random posible item from the loot table (dicionary)
 
-#when they go to the hall they have to fight the bad guys with different weapons including the weapons they have in their pack (saved as dictionary) after that they can turn right or left to go to different rooms (bridge or engine room) function for fight sequence against the security droids can be used later
+#when they go to the hall they have to fight the security droids (three per hall sections) with different weapons including the weapons they have in their pack (saved as dictionary) by typing in a number to acces the different places in their inventory dict. after that they can turn right or left (input as right or left) to go to different rooms (bridge or engine room) function for fight sequence against the security droids can be used later
 
-#else if they go to the engine room the have to solve a different puzzle to turn on the reverse engines giving them more time then they can go to the bridge looping back and letting them go fight the way down the hallways to the hanger for bossfight
+#else if they go to the engine room the have to solve a different puzzle (find the typhoon class submarines captains last name from hunt for red october) if wrong lets them try two more times before they have to wait one minute to turn on the reverse engines giving them more time then they can go to the bridge looping back and letting them go fight the way down the hallways to the hanger for bossfight
 
-#in the hallways there is another loot room (which has same function as previous loot room accsesing dictionary in loot to let them chose to loot or skip and choose a loot item from loot dictionary exept for a weapon if they have it already) they can go to to get more things to help them survive and fight (and to have money for their score) there is always an option to go to a different room that they already cleared
+#in the hallways there is another loot room (which has nearly the same function as previous loot room accsesing dictionary in loot to let them chose to loot or skip and choose a loot item from loot dictionary exept for a weapon if they have it already) they can go to in order to get more things to help them survive and fight (and to have money for their score) there is always an option to go to a different room that they already cleared
 
-#if they go to the bridge they have to solve a sepcific puzzle so the alarm stops and their ship can be freed to go they then have to fight the way down the hallways to the hanger for bossfight on the way there are different security droids and one can drop a acsess card which can be used in the bossfight to acsess a crane to drop a container on the other droids so there is less going on
+#if they go to the bridge they have to solve a sepcific puzzle (trigonometric ratios) if wrong lets them try two more times before they have to wait one minute so the alarm stops and their ship can be freed to go they then have to fight the way down the hallways to the hanger for bossfight on the way there are different security droids and one can drop a acsess card which can be used in the bossfight to acsess a crane to drop a container on the other droids so there is less going on
 
 #if there is sitm pack changes health stat in stats dictionary
 
-#gives puzzle and checks answer if wrong lets them try two more times before they have to wait one minute
-
-#if game is done lets them play again or quit and gives them their score (which is the money they earned plus the time they had left and the amount of enimies destroyed)
+#if game is done lets them play again or quit and gives them their score (which is the money they earned plus the time they took and the amount of enimies destroyed)
