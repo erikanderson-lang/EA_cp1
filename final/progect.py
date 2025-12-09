e5813363842686c6cfc28410b9d6f55f2936089d
erikanderson-lang <erik.anderson@ucas-edu.net>
2025-12-09 09:51:37 -0700

    SURE

--- final/progect.py
+++ final/progect.py
@@ -15,12 +15,11 @@ stats = {
     "accuracy": 3
     }
 invintory = {
-    "main hand": 1,
-    "back pack slot one": 2,
-    "back pack slot two": 3,
-    "back pack slot three": 4
+    " ": 1,
+    " ": 2,
+    " ": 3,
+    " ": 4
     }
-
 print("security systems active.")
 t.sleep(1)
 print("you are looting a star cruiser about to sink in to an electrical storm when the security systems went off. you have twenty muinetes. you now need to get out before the ship sinks.")
@@ -28,13 +27,27 @@ t.sleep(3)
 print("or")
 t.sleep(1)
 print("the security droids destroy you. either way, you need to leave but first you need to go to the bridge and free the docking bay. time starts now.")
-choise = int(input("go to hall(1) or loot storage room (2): ").strip())
-if choise == 1:
-   pass
-elif choise == 2:
-    print (f"you got {r.choice(loot.keys())}.")
-    
+def storeroom():
+    choise = int(input("go to hall(1) or loot storage room (2): ").strip())
+    if choise == 1:
+      pass
+    elif choise == 2:
+       print (f"you got {r.choice(list(loot.keys()))}.")
+    else:
+       print ("invalid choise redo")
+storeroom()
+fightrun = int(input("you are in the hall when you find securitydroids closing in. you can run(1) or fight(2): "))
+if fightrun == 1:
+    runchance = r.randint(1,2)
+    if runchance == 1:
+        print("you got away but ran into other droids who quickly disconected you from the server of life")
+
+    elif runchance == 2:
+       
+       print("")
+
 else:
-    print ("invalid choise redo")
-    
-print("E")
+   print ("invalid choise redo")
+
+
+
