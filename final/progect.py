cab3b24503b22e3cad212f502c1a54ec160fe437
erikanderson-lang <erik.anderson@ucas-edu.net>
2025-12-11 09:52:54 -0700

    SAET

--- final/progect.py
+++ final/progect.py
@@ -10,9 +10,9 @@ loot = {
     "thermal detonator" : 6
     }
 stats = {
-    "health": 1,
-    "wealth": 2,
-    "accuracy": 3
+    "health": 20,
+    "wealth": 0,
+    "damage": 1
     }
 invintory = {
     " ": 1,
@@ -27,15 +27,16 @@ t.sleep(3)
 print("or")
 t.sleep(1)
 print("the security droids destroy you. either way, you need to leave but first you need to go to the bridge and free the docking bay. time starts now.")
+start_time = t.perf_counter()
 def storeroom():
     choise = int(input("go to hall(1) or loot storage room (2): ").strip())
     if choise == 1:
-      pass
+        pass
     elif choise == 2:
-       print (f"you got {r.choice(list(loot.keys()))}.")
+        print (f"you got {r.choice(list(loot.keys()))}.")
+        pass
     else:
        print ("invalid choise redo")
-storeroom()
 
 def hallfight():
     fightrun = int(input("you are in the hall when you find securitydroids closing in. you can run(1) or fight(2): ").strip())
@@ -43,6 +44,7 @@ def hallfight():
         runchance = r.randint(1,2)
         if runchance == 1:
             print("you ran away into other droids who quickly disconected you from the server of life")
+
         elif runchance == 2:
             print("you got away")
         else:
@@ -50,11 +52,41 @@ def hallfight():
     elif fightrun == 2:
         print("")
 
-
     else:
         print ("invalid choise redo")
 
+bridge = 0
+engine = 0
 def hall():
     hallway = int(input("There are two ways to go left bridge(1) or right to other rooms(2): ").strip())
     if hallway == 1:
-        print("you make it to the bridge. You find the consel then words pop up on the screen saying '' ")
+        if bridge == 0:
+            print("you make it to the bridge. You find the consel then words pop up on the screen saying '' ")
+            bridge += int(1)
+
+        else:
+            bridgealready = int(input("there is nothing here for you. go to hall(1): "))
+            if bridgealready == 1:
+                roomchoice = int(input("you are in the hall go to begining store room(1) go to second store room(2) go to engine room(3) or go to hanger(4)" ))
+                if roomchoice == 1:
+                    storeroom()
+                    pass
+                elif roomchoice == 2:
+                    storeroom2()
+                    pass
+                elif roomchoice == 3:
+                    engineroom()
+                    pass
+                elif roomchoice == 4:
+                    hanger()
+                    pass
+                else:
+                    print ("invalid choise redo")
+            else:
+                print ("invalid choise redo")
+    else:
+        print ("invalid choise redo")
+
+storeroom()
+hallfight()
+hall()
